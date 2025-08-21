de6c4353c917a842fa00dc6f22a7119b9cd0eeed
Justin Cordova <justinavodroc@gmail.com>
2025-08-21 18:19:40 -0400

    refactor: requireSupabaseAuth -> requireAuth

--- server/src/middleware/index.ts
+++ server/src/middleware/index.ts
@@ -2,9 +2,7 @@
 // Later when we add more config we split them into individual middleware files
 // Then combine them in middleware/index.ts
 
-export * from "./auth/requireSupabaseAuth";
-export * from "./auth/requireAdmin";
-export * from "./auth/jwtAuth";
+export * from "./auth/requireAuth";
 export * from "./error/errorHandler";
 export * from "./error/notFoundHandler";
 export * from "./error/handleValidationErrors";
