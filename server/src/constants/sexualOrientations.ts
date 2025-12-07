2bfa47c63ba7a57ca8b0be6a72181d688df16013
Justin Cordova <justinavodroc@gmail.com>
2025-12-07 15:33:53 -0500

    chore(constants): expand and reorganize profile setup constants
    Backend:
    - Update genderPreferences: add "Trans Men" and "Trans Women", reorder
    "All" to end
    - Refactor interests: convert from canonical list to client-driven
    organization
    - Expand sexualOrientations with additional options
    - Update constants service exports to include interests endpoint
    - Update seed script with new constants for profile setup

--- server/src/constants/sexualOrientations.ts
+++ server/src/constants/sexualOrientations.ts
@@ -1,16 +1,19 @@
 /**
  * Canonical sexual orientation options for profile setup.
  */
-
 export const SEXUAL_ORIENTATIONS = [
-  "Heterosexual",
-  "Homosexual",
+  "Straight",
+  "Gay",
+  "Lesbian",
   "Bisexual",
   "Pansexual",
   "Asexual",
+  "Demisexual",
   "Queer",
   "Questioning",
+  "Prefer not to say",
   "Other",
 ] as const;
+
 
 export type SexualOrientation = typeof SEXUAL_ORIENTATIONS[number];
